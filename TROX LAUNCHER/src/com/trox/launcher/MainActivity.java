f4fdcbe05a24851f12f110ac5616780482cf344d
COGY_TROX <cogytrox3624@gmail.com>
2026-09-02 23:03:50 +0530

    Update MainActivity - Force landscape mode

--- TROX LAUNCHER/src/com/trox/launcher/MainActivity.java
+++ TROX LAUNCHER/src/com/trox/launcher/MainActivity.java
@@ -3,16 +3,21 @@ package com.trox.launcher;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 import android.widget.Toast;
+import android.content.pm.ActivityInfo;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private Button launchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // Force Landscape Mode
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        
         setContentView(R.layout.activity_main);
 
         launchButton = findViewById(R.id.launch_button);
